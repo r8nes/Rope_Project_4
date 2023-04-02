ccf106d6fa5c508d7f5aefada0cd4501265d02ee
boss.timysn94@outlook.com <boss.timysn94@outlook.com>
2023-04-02 16:46:03 +0300

    - First step of prototyping

---
+++ Assets/@Scripts/Logic/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private float _forceSawToPlayer = 200f;
+
+	[SerializeField] private Transform _sawTransform;
+	[SerializeField] private GameObject _particleForceSaw;
+
+	private Camera _camera;
+	private Rigidbody2D _rb;
+	private LineRenderer _line;
+
+	private void Start()
+	{
+		_camera = Camera.main;
+
+		_rb = GetComponent<Rigidbody2D>();
+		_line = GetComponent<LineRenderer>();
+	}
+
+	private void Update()
+	{
+		_line.SetPosition(0, transform.position);
+		_line.SetPosition(1, _sawTransform.position);
+		
+		Vector3 vector = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+		_rb.position = new Vector3(vector.x, vector.y, 0f);
+		
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Instantiate(_particleForceSaw, _sawTransform.position, Quaternion.identity);
+			Rigidbody2D component = _sawTransform.gameObject.GetComponent<Rigidbody2D>();
+			component.velocity = Vector2.zero;
+			component.AddForce((transform.position - _sawTransform.position).normalized * _forceSawToPlayer);
+		}
+	}
+}
